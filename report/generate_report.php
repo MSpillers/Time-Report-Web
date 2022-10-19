1550eed163f91dbd5aeddad4dd51948e955e3b71
Myles Spillers <43889349+MSpillers@users.noreply.github.com>
2022-10-19 11:28:15 -0500

    Update generate_report.php
    -Added total time calculation function
    -Added calculate_total function
    -Added skel for calulation of transfered com calculation

--- report/generate_report.php
+++ report/generate_report.php
@@ -4,29 +4,68 @@
     <head> 
     <body>
         <?php 
-            pre_r();
+            $array = get_array();
+            print_array($array);
+            $totals = calculate_total($array);
+            print "<p> Total Hours Submitted:" .$totals. "<\p>"; 
         ?>
     <body>
 <html>
 
 <?php 
-function pre_r(){
+function get_array(){
     $user_id = $_POST["ID"];
     $name = $_POST["NAME"];
     $department = $_POST["DEPARTMENT"];
     $sch_hrs = $_POST["SCH-HRS"];
     $rcomp_time = $_POST["RCT"];
     $act_hours = $_POST["ACT-HOURS"];
-    $comp_time = $_POST["COMP-TIME"];
+    $comp_time_used = $_POST["COMP-TIME"];
     $holiday = $_POST["HOL"];
     $jury_duty = $_POST["JD"];
     $mil_duty = $_POST["ML"];
     $leave_wo_pay = $_POST["LWP"];
     $act_hours_acp = $_POST["ACT-HOURS-ACP"];
     $ad_close_leave_period = $_POST["AD-CLP"];
-    $time_report = array($user_id,$name,$department,$sch_hrs,$rcomp_time,$act_hours,$comp_time,$holiday,$jury_duty,$mil_duty,$leave_wo_pay,$act_hours,$act_hours_acp,$ad_close_leave_period);
-    echo '<pre>';
-    print_r($time_report);
+    $array = array($user_id,$name,$department,$sch_hrs,$rcomp_time,$act_hours,$comp_time_used,$holiday,$jury_duty,$mil_duty,$leave_wo_pay,$act_hours_acp,$ad_close_leave_period);
+    return $array;
+}
+
+function print_array($array){
+    echo "<pre>";
+    print_r($array);
     echo '</pre>';
 }
+
+function calculate_total($array){
+    $act_hours = $array[5];
+    $comp_time_used = $array[6];
+    $holiday = $array[7];
+    $jury_duty = $array[8];
+    $mil_duty = $array[9];
+    $leave_wo_pay = $array[10];
+    $ad_close_leave_period = $array[12];
+
+    $totals = $act_hours + $comp_time_used + $holiday + $jury_duty + $mil_duty + $leave_wo_pay + $ad_close_leave_period;
+
+    return $totals;
+}
+
+function calculate_overtime($array){
+    $totals = calculate_total($array);
+    $sch_hrs = $array[3];
+    $overtime = 0;
+
+    if( $totals > $sch_hrs){
+        $overtime = $totals - $sch_hrs;
+    }
+    return $overtime;
+}
+
+function calculate_tr_comp_record(){
+    $overtime;
+}
+
+
+
 ?>
