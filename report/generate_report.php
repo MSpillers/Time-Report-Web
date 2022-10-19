ebfffa76dc7b197a597ffa21a20af4f4aadfd9ab
Myles Spillers <43889349+MSpillers@users.noreply.github.com>
2022-10-19 07:33:04 -0500

    Update to generate_report.php
    -Added array to track variables

--- report/generate_report.php
+++ report/generate_report.php
@@ -4,27 +4,29 @@
     <head> 
     <body>
         <?php 
-            $user_id = $_POST["ID"];
-            $name = $_POST["NAME"];
-            $department = $_POST["DEPARTMENT"];
-            $sch_hrs = $_POST["SCH-HRS"];
-            $rcomp_time = $_POST["RCT"];
-            $act_hours = $_POST["ACT-HOURS"];
-            $comp_time = $_POST["COMP-TIME"];
-            $holiday = $_POST["HOL"];
-            $jury_duty = $_POST["JD"];
-            $mil_duty = $_POST["ML"];
-            $leave_wo_pay = $_POST["LWP"];
-            $act_hours_acp = $_POST["ACT-HOURS-ACP"];
-            $ad_close_leave_period = $_POST["AD-CLP"];
+            pre_r();
         ?>
     <body>
 <html>
 
 <?php 
-function pre_r($array){
+function pre_r(){
+    $user_id = $_POST["ID"];
+    $name = $_POST["NAME"];
+    $department = $_POST["DEPARTMENT"];
+    $sch_hrs = $_POST["SCH-HRS"];
+    $rcomp_time = $_POST["RCT"];
+    $act_hours = $_POST["ACT-HOURS"];
+    $comp_time = $_POST["COMP-TIME"];
+    $holiday = $_POST["HOL"];
+    $jury_duty = $_POST["JD"];
+    $mil_duty = $_POST["ML"];
+    $leave_wo_pay = $_POST["LWP"];
+    $act_hours_acp = $_POST["ACT-HOURS-ACP"];
+    $ad_close_leave_period = $_POST["AD-CLP"];
+    $time_report = array($user_id,$name,$department,$sch_hrs,$rcomp_time,$act_hours,$comp_time,$holiday,$jury_duty,$mil_duty,$leave_wo_pay,$act_hours,$act_hours_acp,$ad_close_leave_period);
     echo '<pre>';
-    print_r($array);
+    print_r($time_report);
     echo '</pre>';
 }
 ?>
