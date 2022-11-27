97937e788da3271377fbbcc3526b6ed45eb6a647
Myles Spillers <43889349+MSpillers@users.noreply.github.com>
2022-11-27 12:09:25 -0600

    Update generate_report.php
    +added function to add submitted values to array for indexing

--- report/generate_report.php
+++ report/generate_report.php
@@ -7,14 +7,17 @@
             
             //retrieve session info
             session_start();
+
+            //payperiod([payperiod_day_array("Sunday","Monday",etc.),payperiod_dates_array("6-5-2022","6-15-2022",etc.)])
             $payperiod = $_SESSION['payperiod'];
             $weeks = $_SESSION['weeks'];
             print_array($payperiod);
             print_array($weeks);
 
+            //hardcoded conversion values
             $conversion = 7.879;
-            $vars = comp_var();
-            $array = get_array($vars);
+            $sub_totals = get_totals_for_sub_cat();
+            $array = get_user_data_array($array);
             $rto_array = to_array();
             print_array($array);
             $totals = calculate_total($array);
@@ -51,14 +54,50 @@
 <?php 
 
 
-function to_array(){
+function get_sub_cat_array(){
     $rto_array = array($_POST["RTO-1"],$_POST["RTO-2"],$_POST["RTO-3"],$_POST["RTO-4"],$_POST["RTO-5"],$_POST["RTO-6"],$_POST["RTO-7"],$_POST["RTO-8"],$_POST["RTO-9"],$_POST["RTO-10"],$_POST["RTO-11"],$_POST["RTO-12"],$_POST["RTO-13"],$_POST["RTO-14"],$_POST["RTO-15"]);
-    return $rto_array;
+    $act_hours_wrk_array = array($_POST["ACT-HOURS-1"],$_POST["ACT-HOURS-2"],$_POST["ACT-HOURS-3"],$_POST["ACT-HOURS-4"],$_POST["ACT-HOURS-5"],$_POST["ACT-HOURS-6"],$_POST["ACT-HOURS-7"],$_POST["ACT-HOURS-8"],$_POST["ACT-HOURS-9"],$_POST["ACT-HOURS-10"],$_POST["ACT-HOURS-11"],$_POST["ACT-HOURS-12"],$_POST["ACT-HOURS-13"],$_POST["ACT-HOURS-14"],$_POST["ACT-HOURS-15"]);
+    $comp_time_used = array($_POST["COMP-TIME-1"],$_POST["COMP-TIME-2"],$_POST["COMP-TIME-3"],$_POST["COMP-TIME-4"],$_POST["COMP-TIME-5"],$_POST["COMP-TIME-6"],$_POST["COMP-TIME-7"],$_POST["COMP-TIME-8"],$_POST["COMP-TIME-9"],$_POST["COMP-TIME-10"],$_POST["COMP-TIME-11"],$_POST["COMP-TIME-12"],$_POST["COMP-TIME-13"],$_POST["COMP-TIME-14"],$_POST["COMP-TIME-15"]);
+    $holiday = array($_POST["HOL-1"],$_POST["HOL-2"],$_POST["HOL-3"],$_POST["HOL-4"],$_POST["HOL-5"],$_POST["HOL-6"],$_POST["HOL-7"],$_POST["HOL-8"],$_POST["HOL-9"],$_POST["HOL-10"],$_POST["HOL-11"],$_POST["HOL-12"],$_POST["HOL-13"],$_POST["HOL-14"],$_POST["HOL-15"]);
+    $med_leave = array($_POST["MED-LEAVE-1"],$_POST["MED-LEAVE-2"],$_POST["MED-LEAVE-3"],$_POST["MED-LEAVE-4"],$_POST["MED-LEAVE-5"],$_POST["MED-LEAVE-6"],$_POST["MED-LEAVE-7"],$_POST["MED-LEAVE-8"],$_POST["MED-LEAVE-9"],$_POST["MED-LEAVE-10"],$_POST["MED-LEAVE-11"],$_POST["MED-LEAVE-12"],$_POST["MED-LEAVE-13"],$_POST["MED-LEAVE-14"],$_POST["MED-LEAVE-15"]);
+    $per_leave = array($_POST["PER-LEAVE-1"],$_POST["PER-LEAVE-2"],$_POST["PER-LEAVE-3"],$_POST["PER-LEAVE-4"],$_POST["PER-LEAVE-5"],$_POST["PER-LEAVE-6"],$_POST["PER-LEAVE-7"],$_POST["PER-LEAVE-8"],$_POST["PER-LEAVE-9"],$_POST["PER-LEAVE-10"],$_POST["PER-LEAVE-11"],$_POST["PER-LEAVE-12"],$_POST["PER-LEAVE-13"],$_POST["PER-LEAVE-14"],$_POST["PER-LEAVE-15"]);
+    $jury_duty = array($_POST["JD-1"],$_POST["JD-2"],$_POST["JD-3"],$_POST["JD-4"],$_POST["JD-5"],$_POST["JD-6"],$_POST["JD-7"],$_POST["JD-8"],$_POST["JD-9"],$_POST["JD-10"] ,$_POST["JD-11"],$_POST["JD-12"],$_POST["JD-13"],$_POST["JD-14"],$_POST["JD-15"]);
+    $mil_duty = array($_POST["ML-1"],$_POST["ML-2"],$_POST["ML-3"],$_POST["ML-4"],$_POST["ML-5"],$_POST["ML-6"],$_POST["ML-7"],$_POST["ML-8"],$_POST["ML-9"],$_POST["ML-10"],$_POST["ML-11"],$_POST["ML-12"],$_POST["ML-13"],$_POST["ML-14"],$_POST["ML-15"]);
+    $leave_wo_pay  = array($_POST["LWP-1"],$_POST["LWP-2"],$_POST["LWP-3"],$_POST["LWP-4"],$_POST["LWP-5"],$_POST["LWP-6"],$_POST["LWP-7"],$_POST["LWP-8"],$_POST["LWP-9"],$_POST["LWP-10"],$_POST["LWP-11"],$_POST["LWP-12"],$_POST["LWP-13"],$_POST["LWP-14"],$_POST["LWP-15"]);
+    $act_hours_acp = array($_POST["ACT-HOURS-ACP-1"],$_POST["ACT-HOURS-ACP-2"],$_POST["ACT-HOURS-ACP-3"],$_POST["ACT-HOURS-ACP-4"],$_POST["ACT-HOURS-ACP-5"],$_POST["ACT-HOURS-ACP-6"],$_POST["ACT-HOURS-ACP-7"],$_POST["ACT-HOURS-ACP-8"],$_POST["ACT-HOURS-ACP-9"],$_POST["ACT-HOURS-ACP-10"],$_POST["ACT-HOURS-ACP-11"],$_POST["ACT-HOURS-ACP-12"],$_POST["ACT-HOURS-ACP-13"],$_POST["ACT-HOURS-ACP-14"],$_POST["ACT-HOURS-ACP-15"]);
+    $ad_close_leave_period = array($_POST["AD-CLP-1"],$_POST["AD-CLP-2"],$_POST["AD-CLP-3"],$_POST["AD-CLP-4"],$_POST["AD-CLP-5"],$_POST["AD-CLP-6"],$_POST["AD-CLP-7"],$_POST["AD-CLP-8"],$_POST["AD-CLP-9"],$_POST["AD-CLP-10"],$_POST["AD-CLP-11"],$_POST["AD-CLP-12"],$_POST["AD-CLP-13"],$_POST["AD-CLP-14"],$_POST["AD-CLP-15"]);
 }
-function comp_var(){
+function get_submitted_values_by_week($weeks,$week){
+    //pass in $weeks and value for week desired .Function should return an array with the submitted totals per week.
+
+    //get the amount of days in each sub week.
+    $wk_one_days = sizeof($weeks[0]);
+    $wk_two_days = sizeof($weeks[1]);
+    $wk_three_days = sizeof($weeks[2]);
+    $wk_four_days = sizeof($weeks[3]);
+
+    //
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//old functions
+
+function get_totals_for_sub_cat(){
     $rto = $_POST["RTO-1"] + $_POST["RTO-2"] + $_POST["RTO-3"] + $_POST["RTO-4"] + $_POST["RTO-5"] + $_POST["RTO-6"] + $_POST["RTO-7"] + $_POST["RTO-8"] + $_POST["RTO-9"] + $_POST["RTO-10"] + $_POST["RTO-11"] + $_POST["RTO-12"] + $_POST["RTO-13"] + $_POST["RTO-14"] + $_POST["RTO-15"];
     $act_hours = $_POST["ACT-HOURS-1"] + $_POST["ACT-HOURS-2"] + $_POST["ACT-HOURS-3"] + $_POST["ACT-HOURS-4"] + $_POST["ACT-HOURS-5"] + $_POST["ACT-HOURS-6"] + $_POST["ACT-HOURS-7"] + $_POST["ACT-HOURS-8"] + $_POST["ACT-HOURS-9"] + $_POST["ACT-HOURS-10"] + $_POST["ACT-HOURS-11"] + $_POST["ACT-HOURS-12"] + $_POST["ACT-HOURS-13"] + $_POST["ACT-HOURS-14"] + $_POST["ACT-HOURS-15"];
-    echo " comp_var act_hours: $act_hours";
     $comp_time_used = $_POST["COMP-TIME-1"] + $_POST["COMP-TIME-2"] + $_POST["COMP-TIME-3"] + $_POST["COMP-TIME-4"] + $_POST["COMP-TIME-5"] + $_POST["COMP-TIME-6"] + $_POST["COMP-TIME-7"] + $_POST["COMP-TIME-8"] + $_POST["COMP-TIME-9"] + $_POST["COMP-TIME-10"] + $_POST["COMP-TIME-11"] + $_POST["COMP-TIME-12"] + $_POST["COMP-TIME-13"] + $_POST["COMP-TIME-14"] + $_POST["COMP-TIME-15"];
     $holiday = $_POST["HOL-1"] + $_POST["HOL-2"] + $_POST["HOL-3"] + $_POST["HOL-4"] + $_POST["HOL-5"] + $_POST["HOL-6"] + $_POST["HOL-7"] + $_POST["HOL-8"] + $_POST["HOL-9"] + $_POST["HOL-10"] + $_POST["HOL-11"] + $_POST["HOL-12"] + $_POST["HOL-13"] + $_POST["HOL-14"] + $_POST["HOL-15"];
     $med_leave = $_POST["MED-LEAVE-1"] + $_POST["MED-LEAVE-2"] + $_POST["MED-LEAVE-3"] + $_POST["MED-LEAVE-4"] + $_POST["MED-LEAVE-5"] + $_POST["MED-LEAVE-6"] + $_POST["MED-LEAVE-7"] + $_POST["MED-LEAVE-8"] + $_POST["MED-LEAVE-9"] + $_POST["MED-LEAVE-10"] + $_POST["MED-LEAVE-11"] + $_POST["MED-LEAVE-12"] + $_POST["MED-LEAVE-13"] + $_POST["MED-LEAVE-14"] + $_POST["MED-LEAVE-15"];
@@ -69,9 +108,9 @@ function comp_var(){
     $act_hours_acp = $_POST["ACT-HOURS-ACP-1"] + $_POST["ACT-HOURS-ACP-2"] + $_POST["ACT-HOURS-ACP-3"] + $_POST["ACT-HOURS-ACP-4"] + $_POST["ACT-HOURS-ACP-5"] + $_POST["ACT-HOURS-ACP-6"] + $_POST["ACT-HOURS-ACP-7"] + $_POST["ACT-HOURS-ACP-8"] + $_POST["ACT-HOURS-ACP-9"] + $_POST["ACT-HOURS-ACP-10"] + $_POST["ACT-HOURS-ACP-11"] + $_POST["ACT-HOURS-ACP-12"] + $_POST["ACT-HOURS-ACP-13"] + $_POST["ACT-HOURS-ACP-14"] + $_POST["ACT-HOURS-ACP-15"];
     $ad_close_leave_period = $_POST["AD-CLP-1"] + $_POST["AD-CLP-2"] + $_POST["AD-CLP-3"] + $_POST["AD-CLP-4"] + $_POST["AD-CLP-5"] + $_POST["AD-CLP-6"] + $_POST["AD-CLP-7"] + $_POST["AD-CLP-8"] + $_POST["AD-CLP-9"] + $_POST["AD-CLP-10"] + $_POST["AD-CLP-11"] + $_POST["AD-CLP-12"] + $_POST["AD-CLP-13"] + $_POST["AD-CLP-14"] + $_POST["AD-CLP-15"];
     $array = array($rto,$act_hours,$comp_time_used,$holiday,$med_leave,$per_leave,$jury_duty,$mil_duty,$leave_wo_pay,$act_hours_acp,$ad_close_leave_period);
-    return $array;
+    return $sub_cat_total_array;
 }
-function get_array($array){
+function get_user_data_array($array){
     $user_id = $_POST["ID"];
     $name = $_POST["NAME"];
     $department = $_POST["DEPARTMENT"];
@@ -88,8 +127,8 @@ function get_array($array){
     $rto = $array[0];
     $act_hours_acp = $array[9];
     $ad_close_leave_period = $array[10];
-    $array = array($user_id,$name,$department,$sch_hrs,$rcomp_time,$act_hours,$comp_time_used,$holiday,$med_leave,$per_leave,$jury_duty,$mil_duty,$leave_wo_pay,$rto,$act_hours_acp,$ad_close_leave_period);
-    return $array;
+    $user_data_array = array($user_id,$name,$department,$sch_hrs,$rcomp_time,$act_hours,$comp_time_used,$holiday,$med_leave,$per_leave,$jury_duty,$mil_duty,$leave_wo_pay,$rto,$act_hours_acp,$ad_close_leave_period);
+    return $user_data_array;
 }
 
 function floor2nearest($number, $decimal) {
